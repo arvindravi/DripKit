46a93caf16daa43c5f153153ebf9adea10765641
Arvind Ravi <arvind.ravi@icloud.com>
2021-11-05 13:52:06 +0530

    Namespace RawResponses

--- Sources/DripKit/WinterOutfitsRawResponse.swift
+++ Sources/DripKit/WinterOutfitsRawResponse.swift
@@ -7,56 +7,59 @@
 
 import Foundation
 
-// MARK: - WinterOutfitsRawResponse
-public struct WinterOutfitsRawResponse: Codable {
-    let userID: Int
-    let outfits: [Outfit]
+extension DripKit {
+    // MARK: - WinterOutfitsRawResponse
+    public struct WinterOutfitsRawResponse: Codable {
+        let userID: Int
+        let outfits: [Outfit]
 
-    enum CodingKeys: String, CodingKey {
-        case userID = "user_id"
-        case outfits
+        enum CodingKeys: String, CodingKey {
+            case userID = "user_id"
+            case outfits
+        }
     }
-}
 
-// MARK: - Outfit
-public struct Outfit: Codable {
-    let when, outfitDescription: String
-    let inspiredByPhotoURL: String
-    let occasion: String
-    let items: [Item]
+    // MARK: - Outfit
+    public struct Outfit: Codable {
+        let when, outfitDescription: String
+        let inspiredByPhotoURL: String
+        let occasion: String
+        let items: [Item]
 
-    enum CodingKeys: String, CodingKey {
-        case when
-        case outfitDescription = "description"
-        case inspiredByPhotoURL = "inspired_by_photo_url"
-        case occasion, items
+        enum CodingKeys: String, CodingKey {
+            case when
+            case outfitDescription = "description"
+            case inspiredByPhotoURL = "inspired_by_photo_url"
+            case occasion, items
+        }
     }
-}
 
-// MARK: - Item
-public struct Item: Codable {
-    let name, brand: String
-    let stockLevel: StockLevel
-    let imageURL: String
-    let url, priceGbp: String
-    let priceGbpSince: Int
-    let pricePreviousGbp: String
-    let pricePreviousGbpSince: Int
+    // MARK: - Item
+    public struct Item: Codable {
+        let name, brand: String
+        let stockLevel: StockLevel
+        let imageURL: String
+        let url, priceGbp: String
+        let priceGbpSince: Int
+        let pricePreviousGbp: String
+        let pricePreviousGbpSince: Int
 
-    enum CodingKeys: String, CodingKey {
-        case name, brand
-        case stockLevel = "stock_level"
-        case imageURL = "image_url"
-        case url
-        case priceGbp = "price_gbp"
-        case priceGbpSince = "price_gbp_since"
-        case pricePreviousGbp = "price_previous_gbp"
-        case pricePreviousGbpSince = "price_previous_gbp_since"
+        enum CodingKeys: String, CodingKey {
+            case name, brand
+            case stockLevel = "stock_level"
+            case imageURL = "image_url"
+            case url
+            case priceGbp = "price_gbp"
+            case priceGbpSince = "price_gbp_since"
+            case pricePreviousGbp = "price_previous_gbp"
+            case pricePreviousGbpSince = "price_previous_gbp_since"
+        }
     }
-}
 
-public enum StockLevel: String, Codable {
-    case inStock = "in_stock"
-    case outOfStock = "out_of_stock"
-}
+    public enum StockLevel: String, Codable {
+        case inStock = "in_stock"
+        case outOfStock = "out_of_stock"
+    }
 
+
+}
--- Tests/DripKitTests/DripKitTests.swift
+++ Tests/DripKitTests/DripKitTests.swift
@@ -2,10 +2,5 @@
     @testable import DripKit
 
     final class DripKitTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(DripKit().text, "Hello, World!")
-        }
+        func testExample() {}
     }
